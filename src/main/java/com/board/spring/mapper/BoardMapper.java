9f3fe91cc43a43201bb3805311e4e122cd486b4f
P-0585\user <iamkang02@i-screamedu.co.kr>
2022-01-10 17:52:43 +0900

    게시판 등록, 수정, 삭제, 조회 기능 추가

---
+++ src/main/java/com/board/spring/mapper/BoardMapper.java
@@ -0,0 +1,28 @@
+package com.board.spring.mapper;
+
+import com.board.spring.domain.BoardDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BoardMapper {
+
+    // 게시글을 생성하는 쿼리를 호출
+    public int insertBoard(BoardDTO params);
+
+    // 하나의 게시글 조회
+    public BoardDTO selectBoardDetail(Long idx);
+
+    // 게시글 수정
+    public int updateBoard(BoardDTO params);
+
+    // 게시글 삭제
+    public int deleteBoard(Long idx);
+
+    // 게시글 전체 리스트 목록 조회
+    public List<BoardDTO> selectBoardList();
+
+    // 삭제여부가 N 으로 지정된 게시글의 개수 조회(페이징 시 필요)
+    public int selectBoardTotalCount();
+}
---
+++ src/main/java/com/board/spring/service/BoardService.java
@@ -0,0 +1,20 @@
+package com.board.spring.service;
+
+import com.board.spring.domain.BoardDTO;
+
+import java.util.List;
+
+public interface BoardService {
+
+    // 생성, 수정
+    public boolean registerBoard(BoardDTO params);
+
+    // 게시글 조회
+    public BoardDTO getBoardDetail(Long idx);
+
+    // 특정 게시글 조회 후 삭제
+    public boolean deleteBoard(Long idx);
+
+    // 전체글 조회
+    public List<BoardDTO> getBoardList();
+}
