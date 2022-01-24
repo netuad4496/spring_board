c902ada698534843fb1dd3a1ef169e7a86cca900
P-0585\user <iamkang02@i-screamedu.co.kr>
2022-01-24 16:44:49 +0900

    댓글 등록, 수정, 삭제 기능 추가

--- src/main/java/com/board/spring/controller/CommentController.java
+++ src/main/java/com/board/spring/controller/CommentController.java
@@ -8,11 +8,9 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,6 +20,27 @@ public class CommentController {
 
     @Autowired
     private CommentService commentService;
+
+    @RequestMapping(value = { "/comments", "/comments/{idx}" }, method = {RequestMethod.POST, RequestMethod.PATCH})
+    public JsonObject registerComment(@PathVariable(value = "idx", required = false) Long idx, @RequestBody final CommentDTO params) {
+
+        JsonObject jsonobj = new JsonObject();
+
+        try {
+            boolean isRegistered = commentService.registerComment(params);
+            jsonobj.addProperty("result", isRegistered);
+
+        } catch (DataAccessException e) {
+            jsonobj.addProperty("message", "데이터베이스 처리 과정 문제가 발생");
+        } catch (Exception e) {
+            jsonobj.addProperty("message", "시스템에 문제가 발생");
+        }
+
+        return jsonobj;
+    }
+
+
+
 
     @GetMapping(value = "/comments/{boardIdx}")
     public JsonObject getCommentList(@PathVariable("boardIdx") Long boardIdx, @ModelAttribute("params") CommentDTO params) {
@@ -37,5 +56,26 @@ public class CommentController {
         }
 
         return jsonObj;
+    }
+
+    @DeleteMapping(value = "/comments/{idx}")
+    public JsonObject deleteComment(@PathVariable("idx") final Long idx) {
+
+        JsonObject jsonObj = new JsonObject();
+
+        try {
+            boolean isDeleted = commentService.deleteComment(idx);
+            jsonObj.addProperty("result", isDeleted);
+
+        } catch (DataAccessException e){
+
+            jsonObj.addProperty("message", "데이터베이스 처리 과정에 문제가 발생하였습니다.");
+
+        } catch (Exception e) {
+            jsonObj.addProperty("message", "시스템에 문제가 발생하였습니다.");
+        }
+
+        return jsonObj;
+
     }
 }
